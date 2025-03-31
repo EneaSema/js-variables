e088392f693c971b52b0d562d5201515d6b334a8
EneaSema <sema.enea1993@gmail.com>
2025-03-31 17:31:30 +0200

    finish exercise bonus

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -6,7 +6,7 @@ const hello = "hello"; // NON MODIFICARE QUESTA RIGA
 // 1. Cerca un carattere in una stringa
 // Stampa l'INDICE con cui viene trovato
 // il carattere Y nella variabile city
-const cityIndex = "";
+const cityIndex = city.indexOf("Y");
 console.log(cityIndex); // Output: 4
 
 // 2. Sottostringhe
@@ -16,7 +16,7 @@ console.log(citySubstring); // Output: "York"
 
 // 3. Sostituzione
 // Sostituisci 'York' con 'Delhi'
-const cityReplaced = "";
+const cityReplaced = city.replace("York", "Delphi");
 console.log(cityReplaced); // Output:  "New Delhi"
 
 // 4. Template literal
